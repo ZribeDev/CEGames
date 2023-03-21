c0eacfe7d93a76fa2549a0cf93f431508d871bb0
ZribeDev <tamirawad10@gmail.com>
2023-03-21 23:33:24 +0200

    added stuff

--- Discord/obj/src/optix.c.d
+++ Discord/obj/src/optix.c.d
@@ -1,28 +1,33 @@
-obj/src/main.c.bc: src/main.c /Users/zribedev/CEdev/include/ti/screen.h \
+obj/src/optix.c.bc: src/optix.c /Users/zribedev/CEdev/include/tice.h \
+  /Users/zribedev/CEdev/include/sys/basicusb.h \
   /Users/zribedev/CEdev/include/stdbool.h \
   /Users/zribedev/CEdev/include/stdint.h \
   /Users/zribedev/CEdev/include/cdefs.h \
-  /Users/zribedev/CEdev/include/stddef.h \
-  /Users/zribedev/CEdev/include/ti/ui.h \
-  /Users/zribedev/CEdev/include/ti/getcsc.h \
-  /Users/zribedev/CEdev/include/stdlib.h \
-  /Users/zribedev/CEdev/include/graphx.h src/gfx/gfx.h \
-  src/gfx/global_palette.h src/gfx/pfp.h src/optix.h \
-  /Users/zribedev/CEdev/include/tice.h \
-  /Users/zribedev/CEdev/include/sys/basicusb.h \
   /Users/zribedev/CEdev/include/sys/lcd.h \
   /Users/zribedev/CEdev/include/sys/power.h \
   /Users/zribedev/CEdev/include/sys/rtc.h \
   /Users/zribedev/CEdev/include/sys/timers.h \
   /Users/zribedev/CEdev/include/sys/util.h \
   /Users/zribedev/CEdev/include/ti/debug.h \
+  /Users/zribedev/CEdev/include/stddef.h \
   /Users/zribedev/CEdev/include/ti/error.h \
   /Users/zribedev/CEdev/include/ti/flags.h \
+  /Users/zribedev/CEdev/include/ti/getcsc.h \
   /Users/zribedev/CEdev/include/ti/getkey.h \
   /Users/zribedev/CEdev/include/ti/graph.h \
   /Users/zribedev/CEdev/include/ti/real.h \
   /Users/zribedev/CEdev/include/ti/info.h \
   /Users/zribedev/CEdev/include/ti/python.h \
+  /Users/zribedev/CEdev/include/ti/screen.h \
+  /Users/zribedev/CEdev/include/ti/ui.h \
   /Users/zribedev/CEdev/include/ti/tokens.h \
-  /Users/zribedev/CEdev/include/ti/vars.h \
-  /Users/zribedev/CEdev/include/keypadc.h
+  /Users/zribedev/CEdev/include/ti/vars.h src/optix.h \
+  /Users/zribedev/CEdev/include/graphx.h \
+  /Users/zribedev/CEdev/include/stdlib.h \
+  /Users/zribedev/CEdev/include/keypadc.h \
+  /Users/zribedev/CEdev/include/stdio.h \
+  /Users/zribedev/CEdev/include/stdarg.h \
+  /Users/zribedev/CEdev/include/string.h \
+  /Users/zribedev/CEdev/include/fileioc.h \
+  /Users/zribedev/CEdev/include/intce.h \
+  /Users/zribedev/CEdev/include/math.h
--- Discord/src/main.c
+++ Discord/src/main.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <graphx.h>
 #include <gfx/gfx.h>
+#include "optix.h"
 void PrintCentered(const char *str);
 
 /* Main function, called first */
@@ -12,13 +13,14 @@ int main(void)
     /* Clear the homescreen */
     os_ClrHome();
     gfx_Begin();    
+    gfx_SetPalette(global_palette, sizeof_global_palette, 0);
     // os_PutStrFull("Hello, World.");
     // while (!os_GetCSC());
     
     
 
     gfx_SetTextFGColor(6);
-    gfx_SetTextBGColor(74);
+    gfx_SetTextBGColor(0);
     gfx_SetTextTransparentColor(5);
     gfx_SetDrawBuffer();
     /* Enable text clipping */
@@ -28,7 +30,7 @@ int main(void)
     /* Draw to buffer to avoid flicker */
     gfx_SetDrawBuffer();
     
-    gfx_FillScreen(74);
+    gfx_FillScreen(0);
     // DRAW LINE
     gfx_SetColor(148);
     int i1 = 0;
@@ -36,11 +38,13 @@ int main(void)
         gfx_FillRectangle(50, i1, 2, 2);
         i1 = i1 + 1;
     }
-
-    gfx_Sprite(pfp, 50, 50);
+    gfx_FillRectangle(51,215,500,2);
+    gfx_Sprite(pfp, 70, 80);
+    gfx_Sprite(pfp, 10, 5);
     // DRAW LINE - END
     gfx_PrintStringXY("CLIPPED TXTEEEE", 70,50);
-
+    optix_Message("Hi", "hello world", 10, 250, 4);
+    char optixmsg = optix_GetStringInput("Message", 10, 500, 15);
     gfx_BlitBuffer();
 
 
